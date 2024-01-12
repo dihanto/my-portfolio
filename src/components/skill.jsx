375283a84ded5b5576308d147042eebaedfe61fd
dihanto <dihanto2306@gmail.com>
2024-01-13 00:41:18 +0700

    ✨ add skill section

---
+++ src/components/skill.jsx
@@ -0,0 +1,17 @@
+export default function Skill() {
+  return (
+    <div className="my-1 mx-5">
+      <h1 className="text-3xl font-bold mb-4">My Coding Journey</h1>
+      <p className="text-base md:text-lg font-medium">
+        I embarked on my programming journey in early 2023, starting from
+        scratch. Initially focusing on backend development, I delved into the
+        intricacies of the Go programming language. Subsequently, I expanded my
+        skill set to encompass frontend technologies, refining my proficiency in
+        HTML, CSS, and JavaScript. This comprehensive approach has equipped me
+        with a well-rounded understanding of both backend and frontend
+        development, enabling me to approach projects with a holistic
+        perspective.
+      </p>
+    </div>
+  );
+}
