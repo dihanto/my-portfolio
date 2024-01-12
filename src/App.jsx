1c96120e6e1e7305e37148a052fdec13a5ea46e1
dihanto <dihanto2306@gmail.com>
2024-01-13 00:41:44 +0700

    ✨ add project sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable react/jsx-no-target-blank */
+import Education from "./components/education";
 import Header from "./components/header";
 import Hero from "./components/hero";
+import Project from "./components/project";
+import Skill from "./components/skill";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Education />
+      <Skill />
+      <Project />
     </>
   );
 }
---
+++ src/components/project.jsx
@@ -0,0 +1,7 @@
+export default function Project() {
+  return (
+    <div className="my-10 mb-80">
+      <h1 className="text-4xl font-bold ml-20  md:ml-[690px]">Project SOON</h1>
+    </div>
+  );
+}
