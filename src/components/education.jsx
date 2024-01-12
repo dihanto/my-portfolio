5d7b7cdb6e9e5d32ac1ce75dc24eae13e4f1a0bf
dihanto <dihanto2306@gmail.com>
2024-01-13 00:40:58 +0700

    ✨ add education section

---
+++ src/components/education.jsx
@@ -0,0 +1,34 @@
+export default function Education() {
+  return (
+    <div className=" ml-5 flex flex-wrap md:flex-nowrap items-start justify-between">
+      <div className="md:pl-16">
+        <h1 className="text-2xl md:text-3xl font-bold my-6">Experience</h1>
+        <h3 className="text-lg md:text-xl font-semibold">
+          {" "}
+          - Operator - PT Mayora Indah
+        </h3>
+        <h3 className="text-lg md:text-xl font-semibold">
+          - Operator - PT Kaldu Sari Nabati
+        </h3>
+        <h3 className="text-lg md:text-xl font-semibold">
+          - Operator - PT Akashi Wahana Indonesia
+        </h3>
+      </div>
+      <div className="md:mx-auto mb-10">
+        <h1 className="text-2xl md:text-3xl font-bold my-6">Education</h1>
+        <h3 className="text-lg md:text-xl font-semibold "> - SDN Cilangkap</h3>
+        <h3 className="text-lg md:text-xl font-semibold ">
+          {" "}
+          - SMPN 1 Puspahiang
+        </h3>
+        <h3 className="text-lg md:text-xl font-semibold ">
+          {" "}
+          - SMKN 2 Tasikmalaya
+        </h3>
+        <h3 className="text-lg md:text-xl font-semibold pl-4">
+          Teknik Komputer dan Jaringan
+        </h3>
+      </div>
+    </div>
+  );
+}
